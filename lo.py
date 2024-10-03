2501c71b4e645ccf8fa5cc6ee884b2e1eb1fdec6
gangaram7877 <147220752+gangaram7877@users.noreply.github.com>
2024-10-03 09:55:52 +0530

    Update lo.py

--- lo.py
+++ lo.py
@@ -70,9 +70,9 @@ def pull_run(work, cmds):
 	        print(result) 
 bot = Client(
   "CW",
-  bot_token="5717007875:AAFuDj6aaKcIT31AZmUQRg2414CcZrjdP-g",
-  api_id=1654363,
-  api_hash="26b911420edb5ceb8f370f21f5eb2684"
+  bot_token="8179116879:AAF2ERstBXz-RnTYeskuZJBB-KVYLPKlLOo",
+  api_id=20913174,
+  api_hash="9b735bf97411081ffc34da861deeae00"
 )
 
 @bot.on_message(filters.command(["down"]) & ~filters.edited)
